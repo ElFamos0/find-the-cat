d051579da17e815b9dfd55c35fcfc826e010cf95
Malo DAMIEN <malo.damien@telecomnancy.eu>
2022-11-26 20:34:34 +0100

    [+] name regexp

--- srcs/name.c
+++ srcs/name.c
@@ -1,5 +1,26 @@
 #include"../includes/name.h"
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <regex.h>
+
+
+
+int regex_check(char * path, char * value) {
+    regex_t regex;
+    int reti;
+    int result;
+    
+    reti = regcomp(&regex, value, 0);
+    if (reti) {
+        fprintf(stderr, "Could not compile regex");
+        exit(1);
+    }
+
+    result = regexec(&regex, path, 0, NULL, 0);
+    regfree(&regex);
+    return result;
+}
+
 
 int get_file_by_name(char * value, path_list * pl) {
     printf("Searching for file %s.\n", value);
@@ -12,21 +33,13 @@ int get_file_by_name(char * value, path_list * pl) {
         struct stat st;
         if (stat(path, &st) == 0) {
             if (S_ISREG(st.st_mode)) {
-                size_t lenstr = strlen(path);
-                size_t lensuffix = strlen(value);
-                if (lenstr >= lensuffix) {
-                    if (strncmp(path + lenstr - lensuffix, value, lensuffix) == 0) {
-                        printf("Found file %s\n", path);}
-                        else{
-                            delete_path(pl, path);
-                            incr = 0;
-                        }
+                if (regex_check(path, value) == 0) {
+                    //printf("Found file %s.\n", path);
                 }
-                else {
+                else{
                     delete_path(pl, path);
                     incr = 0;
                 }
-                
             }
             else {
                 delete_path(pl, path);
@@ -45,3 +58,4 @@ int get_file_by_name(char * value, path_list * pl) {
     }
     
 }
+
